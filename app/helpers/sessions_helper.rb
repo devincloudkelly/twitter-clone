156ca10cbb18888efa5de46d57c83dc797b8fa04
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-12-05 08:21:24 -0600

    Created logged_in? method for SessionsHelper and added comments to methods there

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,19 @@
 module SessionsHelper
+    # Logs in the given user
     def log_in(user)
         session[:user_id] = user.id # 'session' here is a method provided by rails and is automatically encrypted.
     end
 
+    # Returns the current logged-in user (if any)
     def current_user
-        @current_user = @current_user || User.find_by(id: session[:user_id])
+        if session[:user_id]
+            @current_user ||= User.find_by(id: session[:user_id])
+        end
     end
+
+    # Returns true is the user is logged in, false otherwise
+    def logged_in?
+        !current_user.nil?
+    end
+
 end
