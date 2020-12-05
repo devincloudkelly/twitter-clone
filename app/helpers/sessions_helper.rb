72f41d925d090c75c8a80b860a36be3e7fff864d
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-12-05 08:01:20 -0600

    Add current_user method to SessionsHelpers

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,8 @@ module SessionsHelper
     def log_in(user)
         session[:user_id] = user.id # 'session' here is a method provided by rails and is automatically encrypted.
     end
+
+    def current_user
+        @current_user = @current_user || User.find_by(id: session[:user_id])
+    end
 end
