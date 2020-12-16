f07444eb4c8f66894636fb0a6caecec9ecb0ada1
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-12-16 17:03:51 -0600

    Add @other_user fixture, add tests to ensure other user cannot edit or update correct users info

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:devin)
+    @other_user = users(:archer)
   end
 
   test "should get new" do
@@ -28,4 +29,22 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
+  test "should redired edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: {
+      user: {
+        name: @user.name,
+        email: @user.email
+      }
+    }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
 end
