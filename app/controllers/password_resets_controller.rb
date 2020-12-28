85f1868b4ccf57b49710522c6dae51234c3608e3
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-12-28 13:43:18 -0600

    Add check_expiration method to private methods for password_resets_controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -52,5 +52,13 @@ class PasswordResetsController < ApplicationController
       redirect_to root_url
     end
   end
+
+  # Checks expiration of reset token
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danter] = "Password reset has expired."
+      redirect_to new_password_reset_url
+    end
+  end
   
 end
