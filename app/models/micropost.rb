8d989ad8c5017e1c8b700650f80a2375050a0261
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-12-29 15:47:49 -0600

    Add micropost fixtures, write test to fetch microposts by most recent first, and add Proc to Micropost model to ensure latest microposts are returned first

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  # The line below is a Proc/lambda. It is an anonymous function
+  default_scope -> { order(created_at: :desc) } # This sets the default order that we access the database for this model, in this case DESC created_at
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
