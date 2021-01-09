5ce011e91213f0a1eb3603e70decadc3b4c3e468
devincloudkelly <devin.cloud.kelly@gmail.com>
2021-01-09 09:59:30 -0600

    Associate active_storage with micropost model, allowing one image per micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_one_attached :image #This is an active_storage API method, allowing us to attach one image to each micropost
   # The line below is a Proc/lambda. It is an anonymous function
   default_scope -> { order(created_at: :desc) } # This sets the default order that we access the database for this model, in this case DESC created_at
   validates :user_id, presence: true
