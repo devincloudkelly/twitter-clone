8aa73672feb56d82329ac7dffdd523ae77c268c5
devincloudkelly <devin.cloud.kelly@gmail.com>
2021-01-09 11:01:26 -0600

    Add script to _micropost_form to validate image size client-side, fix typo in micropost model image validation

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,7 +5,7 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) } # This sets the default order that we access the database for this model, in this case DESC created_at
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  validagtes :image, content_type: {  in: %w[image/jpeg image/gif image/png],
+  validates :image, content_type: {  in: %w[image/jpeg image/gif image/png],
                                       message: "must be a valid image format(jpeg, gif or png)" },
                       size: { less_than: 5.megabytes,
                               message: "should be less than 5MB"}
