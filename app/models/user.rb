bf11d4394f8a5d5710fcfc43547b272b926ba784
devincloudkelly <devin.cloud.kelly@gmail.com>
2021-01-10 11:18:46 -0600

    Write User methods to follow, unfollow and check following?

--- app/models/user.rb
+++ app/models/user.rb
@@ -76,6 +76,21 @@ class User < ApplicationRecord
     def feed
         Micropost.where('user_id = ?', id) # id is escaped to avoid any SQL injections.
     end
+
+    # Follows a user
+    def follow(other_user)
+        following << other_user
+    end
+
+    # Unfollows a user
+    def unfollow(other_user)
+        following.delete(other_user)
+    end
+
+    # Returns true if the current user is following the other user
+    def following?(other_user)
+        following.include?(other_user)
+    end
     
     private
 
