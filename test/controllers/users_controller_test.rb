d0c17a62c9301e0c2156af360c7cc8d677d06f22
devincloudkelly <devin.cloud.kelly@gmail.com>
2021-01-16 09:58:42 -0600

    Add :following and :followers to before_action in users controller, fix typo in users_controller_test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -87,7 +87,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should redirect followers when not logged in" do
-    get followers_user_path(@users)
+    get followers_user_path(@user)
     assert_redirected_to login_url
   end
 
